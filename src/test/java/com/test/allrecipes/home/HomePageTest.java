9276844f9285b7f0fac4927e8d58ed8d1d1b2926
rompalive <rajesh@rajesh-HP-Laptop-15q-bu0xx>
2022-02-26 16:54:59 +0530

    Added Assertions check

--- src/test/java/com/test/allrecipes/home/HomePageTest.java
+++ src/test/java/com/test/allrecipes/home/HomePageTest.java
@@ -1,6 +1,8 @@
 package com.test.allrecipes.home;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.testng.Assert;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
@@ -17,10 +19,21 @@ public class HomePageTest {
 	}
 
 	@Test
-	public void Chrometest() {
+	public void homePageTitleTest() {
 		driver.get("https://www.allrecipes.com/");
 		driver.manage().window().maximize();
-		System.out.println("The title is " + driver.getTitle());
+		Assert.assertEquals("Allrecipes | Food, friends, and recipe inspiration", "Allrecipes | Food, friends, and recipe inspiration");
+
+	}
+	
+	@Test
+	public void searchBlockTest() throws InterruptedException {
+		String expectedTitle="Recipe Results for Chicken | Allrecipes";
+		driver.findElement(By.id("search-block")).sendKeys("Chicken");
+		driver.findElement(By.id("search-block")).submit();
+		Thread.sleep(5000);
+		String actualTitle=driver.getTitle();
+		Assert.assertEquals(expectedTitle, actualTitle);
 
 	}
 
