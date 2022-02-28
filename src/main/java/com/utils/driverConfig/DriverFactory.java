7d6f8fb2ad2324dace5f7ac4bbfeba0ef23063fb
rompalive <rajesh@rajesh-HP-Laptop-15q-bu0xx>
2022-02-28 20:08:50 +0530

    Removed Physical drivers. Added webdrivermanager to manage drivers

--- src/main/java/com/utils/driverConfig/DriverFactory.java
+++ src/main/java/com/utils/driverConfig/DriverFactory.java
@@ -1,15 +1,19 @@
 package com.utils.driverConfig;
+import static com.utils.constants.ProjectConstants.CHROME;
+import static com.utils.constants.ProjectConstants.FIREFOX;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class DriverFactory {
 
 	static
 	{
-		 System.setProperty("webdriver.chrome.driver","/home/rajesh/git4/Selenium_JavaLeraning/src/main/resources/drivers/chromedriver");
-		 System.setProperty("webdriver.gecko.driver","/home/rajesh/git4/Selenium_JavaLeraning/src/main/resources/drivers/geckodriver");
+		WebDriverManager.chromedriver().setup();
+		WebDriverManager.firefoxdriver().setup();
 	}
     //restrict Instantiation 
     private DriverFactory() {
@@ -17,9 +21,9 @@ public class DriverFactory {
 
     public static WebDriver getDriver(String browser) {
         WebDriver driver = null;
-        if(browser.equals("CHROME")) 
+        if(browser.equals(CHROME)) 
         driver = new ChromeDriver();
-        else if(browser.equals("FIREFOX"))
+        else if(browser.equals(FIREFOX))
         driver= new FirefoxDriver();
         else
         driver=new ChromeDriver(); //defaults to chrome driver if browser fails to match
